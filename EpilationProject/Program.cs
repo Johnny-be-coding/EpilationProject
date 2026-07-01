0e1f47d62efbf0aece17caf55c7c407e545d0d7c
Johnny-be-coding <joninik28@gmail.com>
2026-07-01 08:51:54 +0300

    Added blank windows forms app with .net framework

---
+++ EpilationProject/Program.cs
@@ -0,0 +1,16 @@
+namespace EpilationProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [System.STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
